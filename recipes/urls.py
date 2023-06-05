2eba937695afc56642b50e31847d68f8f14ee34a
RMoreth <ricardoarcemoreth@gmail.com>
2023-06-05 20:39:44 -0300

    dinamic urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 # dominio/recipes/
+
+app_name = 'recipes'
+
 urlpatterns = [
-    path("", views.home),  # Home
-    path("recipes/<int:id>/", views.recipe)
+    path("", views.home, name="home"),  # Home
+    path("recipes/<int:id>/", views.recipe, name="recipe")
 ]
