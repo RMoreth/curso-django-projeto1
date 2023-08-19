59243ebeddaa3b68621b4486a9af18234936b949
RMoreth <ricardoarcemoreth@gmail.com>
2023-08-19 14:23:32 -0300

    Category and Detailpages done

--- recipes/models.py
+++ recipes/models.py
@@ -27,7 +27,7 @@ class Recipe(models.Model):
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL,
-        null=True
+        null=True, blank=True, default=None
     )
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
